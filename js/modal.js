d593a2d8ff3a769c5759d4f5e1c58662039214bb
anya-kashchenko <116516411+anya-kashchenko@users.noreply.github.com>
2023-01-02 17:09:42 +0200

    scss.menu

--- js/modal.js
+++ js/modal.js
@@ -9,8 +9,10 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open")
     refs.modal.classList.toggle("is-hidden");
   }
+
 })();
 
     (() => {
